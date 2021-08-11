d6178d5e44b1a966d5b5d589277547e40f3c0603
deborah-yamazaki <88804621+deborah-yamazaki@users.noreply.github.com>
2021-08-11 20:16:02 -0300

    Update Principal.java
    Alteração no comentário do nome

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -1,9 +1,9 @@
 /*Meu Primeiro Programa Java*
 
 Este programa mostra
-1) Que um programa é construído dentro de um projeto.
-2) Todo programa deve ter uma classe com um método main.
-3) Como exibir informações no terminal (console).
+1) Que um programa Ã© construÃ­do dentro de um projeto.
+2) Todo programa deve ter uma classe com um mÃ©todo main.
+3) Como exibir informaÃ§Ãµes no terminal (console).
 4) Como ler uma string do terminal(console).
 
  */
@@ -15,18 +15,18 @@ import java.util.Scanner;
  * @author Seance
  * Data: 11/08/2021
  */
-public class Principal { // Classe que contém o método principal.
-	public static void main(String[] args) { // Este é o método principal.
+public class Principal { // Classe que contÃ©m o mÃ©todo principal.
+	public static void main(String[] args) { // Este Ã© o mÃ©todo principal.
 		
-		// Pergunta o nome
-		System.out.println("Olá amigo! \nQual é o seu nome?"); // Uso do println
+		// Pergunta o nome usando o println
+		System.out.println("OlÃ¡ amigo! \nQual Ã© o seu nome?");
 		
-		// Lê o nome usando a biblioteca Scanner
+		// LÃª o nome usando a biblioteca Scanner
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Saúda com uma string usando printf
-		System.out.printf("Olá %s! \n", nome);
+		// SaÃºda com uma string usando printf
+		System.out.printf("OlÃ¡ %s! \n", nome);
 	}
 
 }
